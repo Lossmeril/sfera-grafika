fcba785a70980d9340e56dd080abc13db25d5af4
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-10-08 13:54:07 +0100

    feat: logo in header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,16 @@
 import Link from "next/link";
 
-type Link = {
-  displayText: string;
-  link: string;
-};
-
 interface NavbarLinkProps {
   link: string;
   children: React.ReactNode;
+  blank?: boolean;
 }
 
-const NavbarLink: React.FC<NavbarLinkProps> = ({ children, link }) => {
+const NavbarLink: React.FC<NavbarLinkProps> = ({ children, link, blank }) => {
   return (
-    <Link href={link} className="">
+    <Link href={link} className="" target={blank ? "_blank" : ""}>
       <li className="w-fit mx-auto">
-        <p className="underline-link">{children}</p>
+        <span className="underline-link">{children}</span>
       </li>
     </Link>
   );
@@ -22,10 +18,24 @@ const NavbarLink: React.FC<NavbarLinkProps> = ({ children, link }) => {
 
 const Navbar = () => {
   return (
-    <ul className="px-8 py-4 xl:px-20 lg:py-20 text-center">
-      <h3>Jak se to píše?</h3>
+    <ul className="px-8 py-4 lg:py-20 text-center hidden lg:block">
+      <h4>Prvky</h4>
+      <NavbarLink link={"/"}>Sady prvků</NavbarLink>
+      <NavbarLink link={"/"}>Zakázané prvky</NavbarLink>
+
+      <h4 className="mt-10">Jak se to píše?</h4>
       <NavbarLink link={"/copy/sfera"}>SFÉRA</NavbarLink>
       <NavbarLink link={"/copy/datumy"}>Datumy a časy</NavbarLink>
+
+      <h4 className="mt-10">Odkazy na SharePoint</h4>
+      <NavbarLink
+        link={
+          "https://sferapardubice.sharepoint.com/:f:/s/SFERA/Es0LEpuZCf9PkOHb8IMDEosBaZhIxzIB1I0EofrNn9oUzg?e=QRNKWu"
+        }
+        blank
+      >
+        Sady obrázků
+      </NavbarLink>
     </ul>
   );
 };
--- layouts/mainLayout.tsx
+++ layouts/mainLayout.tsx
@@ -11,18 +11,39 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
       <div className="border-b h-[5vh]">
         <div className="border-x h-[5vh] mx-5 sm:mx-10 lg:mx-20"></div>
       </div>
-      <header className="border-b h-auto lg:h-[13vh] flex flex-col justify-center">
-        <div className="px-8 lg:px-20 py-4 lg:py-10 border-x mx-5 sm:mx-10 lg:mx-20">
-          <Link href="/">
-            <h1 className="text-">Manuál pro práci se značkou SFÉRA</h1>
-          </Link>
+
+      {/* --- HLAVIČKA --- */}
+      <header className="border-b h-auto lg:h-[13vh] flex flex-col justify-center overflow-hidden">
+        <div className=" mx-5 sm:mx-10 lg:mx-20 grid grid-cols-5">
+          {/* --- LOGO V HLAVIČCE --- */}
+          <div className="border-b border-x lg:border-b-0 col-span-5 lg:col-span-1 flex flex-col justify-center items-start lg:items-center">
+            <Link href="/">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="/img/logo.svg"
+                alt="logo SFÉRA"
+                className="px-5 lg:px-5 xl:px-10 py-2 lg:py-10 w-[150px] md:w-[250px] lg:w-[300px]"
+              />
+            </Link>
+          </div>
+          {/* --- NÁZEV STRÁNKY --- */}
+          <div className="px-8 lg:px-20 py-4 lg:py-10 flex flex-col justify-center col-span-5 lg:col-span-4 border-x lg:border-l-0 lg:border-r">
+            <Link href="/">
+              <h1 className="">Manuál pro práci se značkou SFÉRA</h1>
+            </Link>
+          </div>
         </div>
       </header>
+
+      {/* --- HLAVNÍ STRÁNKA --- */}
       <main className="">
         <div className="grid grid-cols-5 mx-5 sm:mx-10 lg:mx-20">
           {/* --- BOČNÍ MENU --- */}
           <nav className="col-span-5 border-x border-b lg:col-span-1">
             <Navbar />
+            <div className="flex flex-row items-center w-full h-10 lg:hidden px-8 py-4 lg:py-20">
+              Mobile menu here
+            </div>
           </nav>
 
           {/* --- STRÁNKA --- */}
